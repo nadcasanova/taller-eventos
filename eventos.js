3d3cb42ef7e034f77616d8419ed6206759f41746
NadCasanova <nadia.c0805@gmail.com>
2024-08-15 00:26:53 -0300

    solucion del mensaje

--- eventos.js
+++ eventos.js
@@ -3,3 +3,10 @@ const saludar = document.querySelector(".saludar");
 saludar.addEventListener("click", () => {
     alert("Hola! Soy el div")
 });
+
+const saludarButton = document.querySelector("#saludar");
+
+saludarButton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    alert("Hola!")
+});
